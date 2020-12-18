7d54b90575929dc47f9f5beffa49a9854d2ea91a
Natalia Goulart <natalia.goulart@outlook.com>
2020-12-18 17:43:20 +0900

    Header and Router installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Header />
+
+             {/* TINDER CARDS */}
+             {/* BUTTONS AT BOTTOM*/}
+
+             {/* CHAT SCREEN */}
+             {/* INDIVIDUAL CHAT SCREEN */}
+
+         </Router>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import IconButton from '@material-ui/core/IconButton';
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+            <IconButton>
+                <img
+                    className="header__logo"
+                    src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                    alt="tinder logo"/>
+            </IconButton>
+            <IconButton>
+                <QuestionAnswerIcon className="header__icon" fontSize="large" />
+            </IconButton>
+        </div>
+    );
+}
+
+export default Header;
